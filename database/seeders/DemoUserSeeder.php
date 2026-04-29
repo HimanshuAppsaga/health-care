de47d03b505df0aa331a325930dada2d987dffc9
Himanshu <himanshu.appsaga.io@gmail.com>
2026-04-29 14:45:46 +0530

    feat: create DemoUserSeeder to initialize sample users with assigned roles

--- database/seeders/DemoUserSeeder.php
+++ database/seeders/DemoUserSeeder.php
@@ -53,7 +53,6 @@ class DemoUserSeeder extends Seeder
                 [
                     'name' => $userData['name'],
                     'password' => Hash::make('password'),
-                    'is_active' => true,
                     'phone' => $userData['phone'],
                 ]
             );
